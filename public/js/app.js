a95a62ed8102e3871c746e5ea3bf5097d901cdaa
rcfworks <rcfworks@gmail.com>
2019-07-12 12:32:55 +0800

    Heroku changes

--- public/js/app.js
+++ public/js/app.js
@@ -10,8 +10,8 @@ weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     console.log(search.value);
     display.textContent = search.value;
-    
-    fetch('http://localhost:3050/weather?address='+search.value).then((response)=>{
+
+    fetch('/weather?address='+search.value).then((response)=>{
         response.json().then((data)=>{
             console.log(data);
             display.textContent = JSON.stringify(data);
